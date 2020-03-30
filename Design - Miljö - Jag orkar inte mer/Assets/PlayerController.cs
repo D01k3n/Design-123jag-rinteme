d807238d4c00e8d59bae6482b9e23e80a424c658
Grimenxx <55179254+Grimenxx@users.noreply.github.com>
2020-03-30 10:47:28 +0200

    e

---
+++ Design - Miljö - Jag orkar inte mer/Assets/PlayerController.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    float speed = 10;
+    float sensitivity = 100;
+    Transform camera;
+
+    void Start()
+    {
+        camera = Camera.main.transform;
+    }
+
+
+    void Update()
+    {
+        float horizontal = Input.GetAxis("Horizontal");
+        float vertical = Input.GetAxis("Vertical");
+        transform.Translate(new Vector3(horizontal, 0, vertical) * speed * Time.deltaTime);
+
+        float mouseX = Input.GetAxis("Mouse X");
+        float mouseY = Input.GetAxis("Mouse Y");
+
+        camera.Rotate(-Vector3.right * mouseY * sensitivity * Time.deltaTime);
+        transform.Rotate(transform.up * mouseX * sensitivity * Time.deltaTime);
+
+    }
+}
